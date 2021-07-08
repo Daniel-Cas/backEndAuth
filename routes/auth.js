90ece7da6bddcc580a1f81ef687420f0ff087c06
Daniel-Cas <dmattcomics@gmail.com>
2021-07-08 16:08:15 -0500

    BackEnd para AppAuth Video 14 en curso

---
+++ routes/auth.js
@@ -0,0 +1,29 @@
+
+const { Router } = require('express');
+const { check } = require('express-validator');
+const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+
+const router = Router();
+
+
+
+//Crear un nuevo usuario
+router.post( '/new', crearUsuario);
+
+//Login de usuario
+router.post( '/', [
+    check('email','El email es obligatorio').isEmail(),
+    check('password','La contraseña es obligatoria').isLength({ min: 6}),
+],loginUsuario);
+
+//Validar y revalidar JWT
+router.get( '/renew', revalidarToken );
+
+
+
+
+
+
+module.exports = router;
+
+
